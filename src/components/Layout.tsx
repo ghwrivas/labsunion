a676f54588c59605de50d9c3afb9af6b576f2733
Williams Rivas <williams.rivas1@gmail.com>
2022-11-16 16:00:15 -0300

    fix: navbar import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Navbar from "./NavBar";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
